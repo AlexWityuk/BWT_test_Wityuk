c425db5fbad651dd03c3a9f83632a2328a7bf576
AlexWityuk <noteto@rambler.ru>
2016-03-30 16:36:05 +0300

    AutoLoad1

--- index.php
+++ index.php
@@ -8,11 +8,9 @@ error_reporting(E_ALL);
 
 //2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
-require_once (ROOT.'/components/Router.php');
-//Class Db
-require_once (ROOT.'/components/Db.php');
-//echo ROOT; //Путь к файлу index.php
+define('DS', DIRECTORY_SEPARATOR);
 
-//4  Вызов Router
-$router = new Router();
+require_once (ROOT . DS . 'autoload.php');
+
+$router = new components\Router();
 $router->run();
